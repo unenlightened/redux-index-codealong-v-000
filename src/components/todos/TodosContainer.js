23f7955fb5ff1ff23d10c1331384cc9215247bbb
Chely Ho <adept_inept@yahoo.com>
2018-10-04 00:25:29 +0000

    automatically backed up by learn

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class TodoContainer extends Component {
+
+  render() {
+    return(
+      <div>
+      <div>
+    )
+  }
+}
+
+export default connect()(TodoContainer)
